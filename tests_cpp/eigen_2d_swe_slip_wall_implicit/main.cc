424961a5e263731fb19616a85d8142f25fce2eeb
Chris Wentland <crwentl@sandia.gov>
2023-09-11 13:46:34 -0700

    formatting

--- tests_cpp/eigen_2d_swe_slip_wall_implicit/main.cc
+++ tests_cpp/eigen_2d_swe_slip_wall_implicit/main.cc
@@ -6,38 +6,38 @@
 
 int main()
 {
-  pressio::log::initialize(pressio::logto::terminal);
-  pressio::log::setVerbosity({pressio::log::level::debug});
+    pressio::log::initialize(pressio::logto::terminal);
+    pressio::log::setVerbosity({pressio::log::level::debug});
 
-  namespace pda = pressiodemoapps;
-  const auto meshObj = pda::load_cellcentered_uniform_mesh_eigen(".");
-  constexpr auto order   = pda::InviscidFluxReconstruction::FirstOrder;
-  const auto probId  = pda::Swe2d::SlipWall;
-  int icFlag = 1;
+    namespace pda = pressiodemoapps;
+    const auto meshObj = pda::load_cellcentered_uniform_mesh_eigen(".");
+    constexpr auto order = pda::InviscidFluxReconstruction::FirstOrder;
+    const auto probId  = pda::Swe2d::SlipWall;
+    int icFlag = 1;
 
-  auto appObj = pda::create_problem_eigen(meshObj, probId, order, icFlag);
-  using app_t = decltype(appObj);
-  using state_t = typename app_t::state_type;
-  using jacob_t = typename app_t::jacobian_type;
+    auto appObj = pda::create_problem_eigen(meshObj, probId, order, icFlag);
+    using app_t = decltype(appObj);
+    using state_t = typename app_t::state_type;
+    using jacob_t = typename app_t::jacobian_type;
 
-  state_t state = appObj.initialCondition();
+    state_t state = appObj.initialCondition();
 
-  auto stepperObj = pressio::ode::create_implicit_stepper(
-    pressio::ode::StepScheme::BDF1, appObj);
+    auto stepperObj = pressio::ode::create_implicit_stepper(
+        pressio::ode::StepScheme::BDF1, appObj);
 
-  using lin_solver_t = pressio::linearsolvers::Solver<
-    pressio::linearsolvers::iterative::Bicgstab, jacob_t>;
-  lin_solver_t linSolverObj;
-  auto NonLinSolver = pressio::create_newton_solver(stepperObj, linSolverObj);
-  NonLinSolver.setStopTolerance(1e-5);
+    using lin_solver_t = pressio::linearsolvers::Solver<
+        pressio::linearsolvers::iterative::Bicgstab, jacob_t>;
+    lin_solver_t linSolverObj;
+    auto NonLinSolver = pressio::create_newton_solver(stepperObj, linSolverObj);
+    NonLinSolver.setStopTolerance(1e-5);
 
-  FomObserver<state_t> Obs("swe_slipWall2d_solution.bin", 10);
+    FomObserver<state_t> Obs("swe_slipWall2d_solution.bin", 10);
 
-  const double tf = 5;
-  const double dt = 0.01;
-  const auto Nsteps = pressio::ode::StepCount(tf/dt);
-  pressio::ode::advance_n_steps(stepperObj, state, 0., dt, Nsteps, Obs, NonLinSolver);
+    const double tf = 5;
+    const double dt = 0.01;
+    const auto Nsteps = pressio::ode::StepCount(tf/dt);
+    pressio::ode::advance_n_steps(stepperObj, state, 0., dt, Nsteps, Obs, NonLinSolver);
 
-  pressio::log::finalize();
-  return 0;
+    pressio::log::finalize();
+    return 0;
 }
