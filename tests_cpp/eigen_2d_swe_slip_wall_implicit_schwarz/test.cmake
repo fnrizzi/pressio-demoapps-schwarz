16f22ece09aadb847823204d8a55b14d8807a2b9
Chris Wentland <crwentl@sandia.gov>
2024-01-03 17:02:33 -0800

    update SWE FOM tests

--- tests_cpp/eigen_2d_swe_slip_wall_implicit_schwarz/test.cmake
+++ tests_cpp/eigen_2d_swe_slip_wall_implicit_schwarz/test.cmake
@@ -1,6 +1,6 @@
 include(FindUnixCommands)
 
-set(CMD "python3 ${DECOMPDRIVER} --meshScript ${MESHDRIVER} -n 50 50 --outDir ${OUTDIR} -s ${STENCILVAL} --bounds -5.0 5.0 -5.0 5.0 --numDoms 2 2 --overlap 10")
+set(CMD "python3 ${DECOMPDRIVER} --meshScript ${MESHDRIVER} -n 30 30 --outDir ${OUTDIR} -s ${STENCILVAL} --bounds -5.0 5.0 -5.0 5.0 --numDoms 2 2 --overlap 6")
 message(NOTICE ${CMD})
 execute_process(COMMAND ${BASH} -c ${CMD} RESULT_VARIABLE RES)
 if(RES)
