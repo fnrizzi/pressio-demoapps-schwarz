0384e02b53a749ad6ab5965a41b5199f77e33563
Chris Wentland <crwentl@sandia.gov>
2024-01-09 09:49:38 -0800

    remove usage of create_decomp_sample_meshes.py, which is no longer needed

--- tests_cpp/eigen_2d_swe_slip_wall_implicit_hproms_schwarz/lspg/main.cc
+++ tests_cpp/eigen_2d_swe_slip_wall_implicit_hproms_schwarz/lspg/main.cc
@@ -48,7 +48,10 @@ int main()
     // tiling, meshes, and decomposition
     auto tiling = std::make_shared<pdas::Tiling>(meshRootFull);
     auto [meshObjsFull, meshPathsFull] = pdas::create_meshes(meshRootFull, tiling->count());
-    auto [meshObjsHyper, meshPathsHyper] = pdas::create_meshes(meshRootHyper, tiling->count());
+    std::vector<std::string> meshPathsHyper;
+    for (int domIdx = 0; domIdx < meshPathsFull.size(); ++ domIdx) {
+        meshPathsHyper.emplace_back(meshRootHyper + "/domain_" + std::to_string(domIdx));
+    }
     auto subdomains = pdas::create_subdomains<app_t>(
         meshObjsFull, *tiling, probId, scheme, order,
         domFlagVec, transRoot, basisRoot, nmodesVec, icFlag,
